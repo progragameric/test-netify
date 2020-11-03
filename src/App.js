5ce19ed57ab34930f66d393b8ee6c8132c268762
Fung Kin Chun <fungkinchun@Fungs-MacBook-Pro.local>
2020-11-03 23:57:57 +0800

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
+import logg from './logg.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logg} className="App-logo" alt="logo" />
+        <h1>
+          Progragameric
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I wrote code to code
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:progragameric@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Email
         </a>
       </header>
     </div>
